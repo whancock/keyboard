00fc221fe2986d60a753c26db2d8594a964a597c
Will <hack@willhancock.io>
2020-10-31 04:39:09 +0000

    playing with firmware

--- firmware.py
+++ firmware.py
@@ -1,13 +1,58 @@
-import asynchio
-from evdev import UInput, ecodes as e
+#!/usr/bin/env python3
 
-ui = UInput()
-
-
-ui.write(e.EV_KEY, e.KEY_A, 1)  # KEY_A down
-ui.write(e.EV_KEY, e.KEY_A, 0)  # KEY_A up
-ui.syn()
+#import asyncio
+import evdev
+#from evdev import UInput, ecodes as e
 
 
-ui.close()
+#keybd = evdev.InputDevice('/dev/hidg0')
+
+
+
+#print(evdev.list_devices())
+
+# from rmedgar.com
+NULL_CHAR = chr(0)
+
+def write_report(report):
+	with open('/dev/hidg0', 'rb+') as fd:
+		fd.write(report.encode())
+
+# e
+write_report(NULL_CHAR*2+chr(8)+NULL_CHAR*5)
+write_report(NULL_CHAR*2+chr(128)+NULL_CHAR*5)
+
+# ! (press shift and 1)
+# write_report(chr(32)+NULL_CHAR+chr(30)+NULL_CHAR*5)
+
+# Release all keys
+write_report(NULL_CHAR*8)
+
+
+
+# from cherry pi split keyboard
+# can report multiple keys per tick, may need this later
+#def report_keyboard(c,k):
+#    with open('/dev/hidg0', 'rb+') as fk:
+#        fk.write((c).to_bytes(1, byteorder='big')+    \
+#                 (0).to_bytes(1, byteorder='big')+    \
+#                 (k[0]).to_bytes(1, byteorder='big')+ \
+#                 (k[1]).to_bytes(1, byteorder='big')+ \
+#                 (k[2]).to_bytes(1, byteorder='big')+ \
+#                 (k[3]).to_bytes(1, byteorder='big')+ \
+#                 (k[4]).to_bytes(1, byteorder='big')+ \
+#                 (k[5]).to_bytes(1, byteorder='big'))
+
+
+
+
+#ui = UInput()
+
+
+#ui.write(e.EV_KEY, e.KEY_A, 1)  # KEY_A down
+#ui.write(e.EV_KEY, e.KEY_A, 0)  # KEY_A up
+#ui.syn()
+
+
+#ui.close()
 
