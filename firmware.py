4ebcfd145ee4a31778776d6d1bcea5c113a70ba9
Will <hack@willhancock.io>
2020-11-02 00:54:56 +0000

    modifying firmware to handle button and rotation

--- firmware.py
+++ firmware.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-#import asyncio
 import asyncio, evdev
-#from evdev import UInput, ecodes as e
 
-
-encoder = evdev.InputDevice('/dev/input/event0')
-#keybd = evdev.InputDevice('/dev/hidg0')
+encoder = evdev.InputDevice('/dev/input/by-path/platform-soc:master-event')
+button = evdev.InputDevice('/dev/input/by-path/platform-soc:mute-event-joystick')
 
 # from rmedgar.com
 NULL_CHAR = chr(0)
@@ -28,17 +25,18 @@ def write_report(report):
 
 async def print_events(device):
 	async for event in device.async_read_loop():
-		print(evdev.categorize(event), event.type, event.code, event.value, sep=': ')
+		# print(evdev.categorize(event), event.type, event.code, event.value, sep=': ')
 		if event.type == evdev.ecodes.EV_REL and event.code == evdev.ecodes.REL_X:
-			print('Rel event')
+			# print('Rel event')
 			if event.value == 1:
 				write_report(NULL_CHAR * 2 + chr(1) + NULL_CHAR * 5)
 			elif event.value == -1:
-				print('Negative event')
+				# print('Negative event')
 				write_report(NULL_CHAR * 2 + chr(2) + NULL_CHAR * 5)
 		# print(device.path, evdev.categorize(event), sep=': ')
 
-asyncio.ensure_future(print_events(encoder))
+for device in encoder, button:
+	asyncio.ensure_future(print_events(device))
 
 loop = asyncio.get_event_loop()
 loop.run_forever()
